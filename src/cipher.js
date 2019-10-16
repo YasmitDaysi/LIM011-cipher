b6764339281a741110a977647337cff6349ea260
YasmitDaysi <yasdayvasand98@gmail.com>
2019-10-16 09:23:03 -0500

    Editado

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,18 @@
-
 window.cipher = {
   encode: (offset, string) => {
     let resultadoCipher = '';
     for (let i = 0; i < string.length; i += 1) {
       const numberCodeAscii = string.charCodeAt(i);
       if (numberCodeAscii >= 65 && numberCodeAscii <= 90) {
-        resultadoCipher += String.fromCharCode(((numberCodeAscii - 65 + parseInt(offset, 10)) % 26) + 65);
+        resultadoCipher
+        += String.fromCharCode(((numberCodeAscii - 65 + parseInt(offset, 10)) % 26) + 65);
       } else if (numberCodeAscii >= 97 && numberCodeAscii <= 122) {
-        resultadoCipher += String.fromCharCode(((numberCodeAscii - 97 + parseInt(offset, 10)) % 26) + 97);
+        resultadoCipher
+        += String.fromCharCode(((numberCodeAscii - 97 + parseInt(offset, 10)) % 26) + 97);
       } else if (numberCodeAscii >= 48 && numberCodeAscii <= 57) {
-        resultadoCipher += String.fromCharCode(((numberCodeAscii - 48 + parseInt(offset, 10)) % 10) + 48);
-      } 
-      else { 
-        resultadoCipher += string.charAt(i)
-      }
+        resultadoCipher
+        += String.fromCharCode(((numberCodeAscii - 48 + parseInt(offset, 10)) % 10) + 48);
+      } else { resultadoCipher += string.charAt(i); }
     } return resultadoCipher;
   },
 
@@ -30,10 +29,7 @@ window.cipher = {
       } else if (numberCodeAscii >= 48 && numberCodeAscii <= 57) {
         resultadoDecifrado
         += String.fromCharCode(((numberCodeAscii - 57 - parseInt(offset, 10)) % 10) + 57);
-      } 
-      else { 
-        resultadoDecifrado += string.charAt(i); 
-      }
+      } else { resultadoDecifrado += string.charAt(i); }
     } return resultadoDecifrado;
   },
 };
